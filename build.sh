c062b545d771343aa10cf935913affc693f6ef97
Jarvis <mikeyjar1@gmail.com>
2025-05-20 23:11:09 -0400

    Build more languages

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@ PROTO_DIR="./proto"
 GO_OUT="./go/patronobuf"
 PY_OUT="./python/patronobuf"
 CS_OUT="./csharp/patronobuf"
+JAVA_OUT="./java/patronobuf"
+JS_OUT="./js/patronobuf"
+TS_OUT="./ts/patronobuf"
+RB_OUT="./ruby/patronobuf"
 
 # Ensure output directories exist
-mkdir -p "$GO_OUT" "$PY_OUT" "$CS_OUT"
+mkdir -p "$GO_OUT" "$PY_OUT" "$CS_OUT" "$JAVA_OUT" "$JS_OUT" "$TS_OUT"
 
 echo "[*] Generating Go code..."
 
@@ -21,24 +25,46 @@ docker run --rm -v "$PWD":/work -w /work proto-gen \
   --go-grpc_opt=paths=source_relative \
   --proto_path=proto proto/agents.proto
 
-echo "[*] Initializing go.mod for Go package..."
-
-docker run --rm -v "$PWD/go/patronobuf:/goout" -w /goout golang:1.24.3 \
-  sh -c "go mod init github.com/PatronC2/Patronobuf/go/patronobuf && go mod tidy"
-
-
 echo "[*] Generating Python code..."
 docker run --rm -v "$(pwd):/defs" -w /defs \
   namely/protoc-all \
   -d "$PROTO_DIR" \
   -l python \
-  -o python/patronobuf
+  -o "$PY_OUT"
 
 echo "[*] Generating C# code..."
 docker run --rm -v "$(pwd):/defs" -w /defs \
   namely/protoc-all \
   -d "$PROTO_DIR" \
   -l csharp \
-  -o csharp/patronobuf
+  -o "$CS_OUT"
 
-echo "[✓] Protobuf generation and go.mod complete."
+echo "[*] Generating Java code..."
+docker run --rm -v "$(pwd):/defs" -w /defs \
+  namely/protoc-all \
+  -d "$PROTO_DIR" \
+  -l java \
+  -o "$JAVA_OUT"
+
+echo "[*] Generating JavaScript code..."
+docker run --rm -v "$(pwd):/defs" -w /defs \
+  namely/protoc-all \
+  -d "$PROTO_DIR" \
+  -l node \
+  -o "$JS_OUT"
+
+echo "[*] Generating TypeScript code..."
+docker run --rm -v "$(pwd):/defs" -w /defs \
+  namely/protoc-all \
+  -d "$PROTO_DIR" \
+  -l typescript \
+  -o "$TS_OUT"
+
+echo "[*] Generating Ruby code..."
+docker run --rm -v "$(pwd):/defs" -w /defs \
+  namely/protoc-all \
+  -d "$PROTO_DIR" \
+  -l ruby \
+  -o "$RB_OUT"
+
+echo "[✓] Protobuf generation complete."
